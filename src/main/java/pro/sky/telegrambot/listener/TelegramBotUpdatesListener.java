68f12748f51522ae51d4aa601dce621034aa1e9b
Alemakave <savercat4@gmail.com>
2024-02-05 00:50:53 +1000

    Task 2 done

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -7,10 +7,19 @@ import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.exception.TaskFormatException;
+import pro.sky.telegrambot.model.NotificationTask;
+import pro.sky.telegrambot.repository.NotificationTaskRepository;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -19,6 +28,8 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Autowired
     private TelegramBot telegramBot;
+    @Autowired
+    private NotificationTaskRepository notificationTaskRepository;
 
     @PostConstruct
     public void init() {
@@ -34,9 +45,46 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             long chatId = update.message().chat().id();
             if (update.message().text().equals("/start")) {
                 telegramBot.execute(new SendMessage(chatId, String.format("Привет %s!", update.message().chat().username())));
+            } else {
+                try {
+                    NotificationTask task = parseNotificationTask(chatId, update.message().text());
+                    notificationTaskRepository.save(task);
+                    telegramBot.execute(new SendMessage(chatId, "Задача добавлена на " + task.getSendMessageDateTime().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))));
+                } catch (TaskFormatException e) {
+                    telegramBot.execute(new SendMessage(chatId, e.getMessage()));
+                }
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+    public NotificationTask parseNotificationTask(long chatId, String message) throws TaskFormatException {
+        Pattern pattern = Pattern.compile("([0-9.:\\s]{16})(\\s)([\\W\\w+]+)");
+        Matcher matcher = pattern.matcher(message);
+
+        if (!matcher.matches()) {
+            throw new TaskFormatException("Сообщение имеет некорректное форматирование. Необходимо чтоб сообщение было вида: 01.01.2022 20:00 Сделать домашнюю работу");
+        }
+
+        String dateTimeAsText = matcher.group(1);
+        String notificationText = matcher.group(3);
+
+        LocalDateTime dateTime = LocalDateTime.parse(dateTimeAsText, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+
+        NotificationTask task = new NotificationTask();
+        task.setChatId(chatId);
+        task.setTaskText(notificationText);
+        task.setSendMessageDateTime(dateTime);
+
+        return task;
+    }
+
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void findAndSendNotification() {
+        LocalDateTime dateTime = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+        NotificationTask notificationTask = notificationTaskRepository.findBySendMessageDateTime(dateTime);
+        if (notificationTask != null) {
+            telegramBot.execute(new SendMessage(notificationTask.getChatId(), notificationTask.getTaskText()));
+        }
+    }
 }
